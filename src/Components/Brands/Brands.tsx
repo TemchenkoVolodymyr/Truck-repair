c379109a1c90d2465086ffa01510fed26a8b4953
TemchenkoVolodymyr <vovatemcenko4@gmail.com>
2023-11-01 18:53:47 +0100

    created section for brands

---
+++ src/Components/Brands/Brands.tsx
@@ -0,0 +1,36 @@
+import style from './Brands.module.scss'
+import scania from '../../assets/scania.svg'
+import daf from '../../assets/daf.svg'
+import man from '../../assets/man.svg'
+import renault from '../../assets/renault.png'
+import volvo from '../../assets/volvo.png'
+
+const Brands = () => {
+    return (
+        <section className={style.container}>
+            <div className={style.info}>
+                <h1>С якими брендами мы працюэмо</h1>
+                <p>Ми працюємо з найпопулярнішими брендами, такими як SCANIA, VOLVO, MAN, RENAULT, DAF та багатьма
+                    іншими популярними моделями!</p>
+                <p>Ми працюємо з цими брендами щодня!
+                    Найкраща якість обслуговування!
+                    У нас найдосвідченіші працівники в місті!</p>
+            </div>
+            <div className={style.brands}>
+                <div className={style.blockImage}>
+                    <img src={scania} alt={'brand'}/>
+                    <img src={volvo} alt={'brand'}/>
+                </div>
+                <div className={style.image}>
+                    <img src={man} alt={'brand'}/>
+                </div>
+                <div className={style.blockImage}>
+                    <img src={daf} alt={'brand'}/>
+                    <img src={renault} alt={'brand'}/>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Brands;
--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,10 +1,12 @@
 import style from './Home.module.scss'
 import StartSection from "./StartSection/StartSection.tsx";
+import Brands from "../Brands/Brands.tsx";
 
 const Home = () => {
     return (
         <article className={style.container}>
             <StartSection></StartSection>
+            <Brands></Brands>
         </article>
     );
 };
