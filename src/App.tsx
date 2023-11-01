c444398364e0e118108a69ed4082c3c33ef68a28
TemchenkoVolodymyr <vovatemcenko4@gmail.com>
2023-11-01 18:18:03 +0100

    added main image and create Home Section

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import {Route, Routes} from "react-router-dom";
 import Layout from "./Router/Layout/Layout.tsx";
 import './App.css'
+import Home from "./Components/Home/Home.tsx";
 
 function App() {
 
@@ -8,7 +9,7 @@ function App() {
         <>
             <Routes>
                 <Route path={'/'} element={<Layout/>}>
-
+                    <Route index element={<Home/>}></Route>
                 </Route>
             </Routes>
         </>
---
+++ src/Components/Home/Home.tsx
@@ -0,0 +1,20 @@
+import style from './Home.module.scss'
+import CustomButton from "../../Helpers/CustomButton/CustomButton.tsx";
+import trucks from '../../assets/trucks.png'
+
+const Home = () => {
+    return (
+        <article className={style.container}>
+            <section className={style.title}>
+                <h1>Ремонт вантажiвок </h1>
+                <p>Наша майстерня пропонує найкращу якість обслуговування та комфорт</p>
+                <CustomButton title={"Отримати послугу"}/>
+            </section>
+            <section className={style.image}>
+                <img src={trucks} alt={'truck'}/>
+            </section>
+        </article>
+    );
+};
+
+export default Home;
