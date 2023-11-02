a7aa1c50420a700c1791eb5086506b32ab4b035b
Ley official <danyilfreelance@gmail.com>
2023-11-02 16:11:31 +0000

    changed-style-for-services

---
+++ src/Pages/Services/List.ts
@@ -0,0 +1,14 @@
+import wheel from './../../img/ServicesImg/450-4504112_engine-carens-automotive-motors-parts-kia-cerato-clipart 1.png'
+import filter from './../../img/ServicesImg/PngItem_1125131 1.jpg'
+import oil from './../../img/ServicesImg/accef6ee4240381543d482643a7b4e99 1.png'
+export const ServicesList = [
+    {title : 'Б/у запчасти для грузовых авто из Европы' ,
+        photo : wheel
+    },
+    {title : 'Магазин аксессуаров' ,
+        photo : filter
+    },
+    {title : 'Пункт замены масла грузовых авто' ,
+        photo : oil
+    },
+]
