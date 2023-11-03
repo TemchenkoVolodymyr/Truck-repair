70dbe5d9188bd8500b0c0181f354575baf3a608b
TemchenkoVolodymyr <vovatemcenko4@gmail.com>
2023-11-03 18:16:18 +0100

    added new logo and button scroll up

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -7,8 +7,27 @@ import Map from "../../Pages/Map/Map.tsx";
 import Footer from "../../Pages/Footer/Footer.tsx";
 
 import ContactUs from "../ContactUs/ContactUs.tsx";
+import {AiOutlineArrowUp} from "react-icons/ai";
+import {useEffect, useState} from "react";
+import {Link} from "react-scroll";
 
 const Home = () => {
+    const [showScrollUp,setShowScrollUp] = useState<boolean>(false)
+
+    useEffect(() => {
+        window.addEventListener('scroll',handleScroll);
+    },[window.pageYOffset])
+
+    const handleScroll = () => {
+        if (window.pageYOffset > 800) {
+            setShowScrollUp(true);
+        } else {
+            setShowScrollUp(false);
+        }
+    };
+
+
+
     return (
         <article className={style.container}>
             <StartSection></StartSection>
@@ -18,6 +37,12 @@ const Home = () => {
             <Map/>
             <ContactUs></ContactUs>
             <Footer/>
+
+            {showScrollUp ? <Link  to={'header'} className={style.moveUp}>
+
+                <AiOutlineArrowUp></AiOutlineArrowUp>
+
+            </Link> : null}
         </article>
     );
 };
