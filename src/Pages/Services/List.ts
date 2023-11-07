3bfeb0e04818c3476d3ed53f60a55ddc4bfa7b02
TemchenkoVolodymyr <vovatemcenko4@gmail.com>
2023-11-07 10:30:15 +0100

    added new image

--- src/Pages/Services/List.ts
+++ src/Pages/Services/List.ts
@@ -1,18 +1,21 @@
 import wheel from './../../img/ServicesImg/450-4504112_engine-carens-automotive-motors-parts-kia-cerato-clipart 1.png'
 import filter from './../../img/ServicesImg/PngItem_1125131 1.jpg'
 import oil from './../../img/ServicesImg/accef6ee4240381543d482643a7b4e99 1.png'
+import to from '../../assets/to.png'
+import svarka from '../../assets/svarka.png'
+import filter from '../../assets/filter.png'
 export const ServicesList = [
     {
-        title: 'Б/в запчастини для вантажних автомобілів з Європи',
-        photo: wheel
+        title: 'ТО та діагностика усіх систем',
+        photo: to
     },
     {
-        title: 'Токарні роботи',
+        title: 'Зварювальні роботи',
+        photo: svarka
+    },
+    {
+        title: 'Заміна фільтрів, масла двигуна, КПП, мостів трансмісії',
         photo: filter
-    },
-    {
-        title: 'Точка заміни масла для вантажівок',
-        photo: oil
     },
 ]
 
